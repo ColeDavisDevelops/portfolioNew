4589da3eea1ccf61cf0fde230d908e8c67f4a733
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-09-02 14:43:29 -0500

    added links

--- src/container/Blogs.js
+++ src/container/Blogs.js
@@ -9,6 +9,9 @@ const Blogs = () => {
     },
     description: {
       color: 'rgb(170, 170, 170)'
+    },
+    link: {
+      textDecoration: 'none'
     }
   }
   return (
@@ -47,48 +50,56 @@ const Blogs = () => {
         style={{display: 'block', margin: 'auto'}}
       >
         <Carousel.Item>
-          <img
-            className="d-block w-100 image"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/PokerStars_emblem.svg/1200px-PokerStars_emblem.svg.png"
-            alt="Poker"
-          />
-          <Carousel.Caption>
-            <h3>Screen Scraping for Real-time Poker Data</h3>
-            <p style={classes.description}>The plan is to take a screenshot of the cards dealt to me on poker stars. Convert the screenshots into a hash and manually map the hashes to their values in a python dictionary.</p>
-          </Carousel.Caption>
+          <a style={classes.link} href="https://medium.com/@colebuildanddevelop/screen-scraping-for-real-time-poker-data-25053f572bab" target="_blank">
+            <img
+              className="d-block w-100 image"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/PokerStars_emblem.svg/1200px-PokerStars_emblem.svg.png"
+              alt="Poker"
+            />
+            <Carousel.Caption>
+              <h3>Screen Scraping for Real-time Poker Data</h3>
+              <p style={classes.description}>The plan is to take a screenshot of the cards dealt to me on poker stars. Convert the screenshots into a hash and manually map the hashes to their values in a python dictionary.</p>
+            </Carousel.Caption>
+          </a>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100 image"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Icon-Vim.svg/768px-Icon-Vim.svg.png"
-            alt="Vim"
-          />
-          <Carousel.Caption>
-            <h3>Why I am Learning Vim</h3>
-            <p style={classes.description}>Vim is a highly configurable text editor built to enable efficient text editing. It is an improved version of the vi editor distributed…</p>
-          </Carousel.Caption>
+          <a style={classes.link} href="https://medium.com/@colebuildanddevelop/i-am-learning-vim-2883fb0565e4" target="_blank">
+            <img
+              className="d-block w-100 image"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Icon-Vim.svg/768px-Icon-Vim.svg.png"
+              alt="Vim"
+            />
+            <Carousel.Caption>
+              <h3>Why I am Learning Vim</h3>
+              <p style={classes.description}>Vim is a highly configurable text editor built to enable efficient text editing. It is an improved version of the vi editor distributed…</p>
+            </Carousel.Caption>
+          </a>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100 image"
-            src="https://image.flaticon.com/icons/svg/2948/2948410.svg"
-            alt="Two Generals"
-          />
-          <Carousel.Caption>
-            <h3>The Flaws of Communication</h3>
-            <p style={classes.description}>Imagine two groups of allied gangs each on opposing sides of a city (A and B). In between them is a rival gang(C). The outer city gangs…</p>
-          </Carousel.Caption>
+          <a style={classes.link} href="https://medium.com/@colebuildanddevelop/the-flaws-of-communication-e640f9b78420" target="_blank">
+            <img
+              className="d-block w-100 image"
+              src="https://image.flaticon.com/icons/svg/2948/2948410.svg"
+              alt="Two Generals"
+            />
+            <Carousel.Caption>
+              <h3>The Flaws of Communication</h3>
+              <p style={classes.description}>Imagine two groups of allied gangs each on opposing sides of a city (A and B). In between them is a rival gang(C). The outer city gangs…</p>
+            </Carousel.Caption>
+          </a>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100 image"
-            src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
-            alt="Node Js"
-          />
-          <Carousel.Caption>
-            <h3>Quick Start: Node.Js, Express, MongoDB</h3>
-            <p style={classes.description}>Welcome Developers, I am going to explain how to get started with Node.js, Express and MongoDb. I won’t go into depth explaining the code…</p>
-          </Carousel.Caption>
+          <a style={classes.link} href="https://medium.com/swlh/quick-start-node-js-express-mongodb-47465510bd01" target="_blank">
+            <img
+              className="d-block w-100 image"
+              src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
+              alt="Node Js"
+            />
+            <Carousel.Caption>
+              <h3>Quick Start: Node.Js, Express, MongoDB</h3>
+              <p style={classes.description}>Welcome Developers, I am going to explain how to get started with Node.js, Express and MongoDb. I won’t go into depth explaining the code…</p>
+            </Carousel.Caption>
+          </a>
         </Carousel.Item>
       </Carousel>
     </div>
